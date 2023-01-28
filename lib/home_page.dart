fbc2f4414f03848f901e6a4f3e184d7009d2da76
Juwel Sheikh <jewelshekh1996@gmail.com>
2023-01-28 23:41:06 +0600

    2nd button add

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,9 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+
+import 'final_page.dart';
+
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -9,31 +12,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int rightDiceNumber=1;
-  int leftDiceNumber=3;
+  int rightDiceNumber = 1;
+  int leftDiceNumber = 3;
 
-
-  void rightLudo(){
+  void rightLudo() {
     setState(() {
-      rightDiceNumber=Random().nextInt(6)+1;
-    });
-
-  }
-  void leftLudo(){
-    setState(() {
-      leftDiceNumber=Random().nextInt(6)+1;
+      rightDiceNumber = Random().nextInt(6) + 1;
     });
   }
+
+  void leftLudo() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.indigo,
         body: Container(
           color: Colors.teal,
           margin: EdgeInsets.all(15),
-
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
@@ -41,41 +42,35 @@ class _HomePageState extends State<HomePage> {
               Row(
                 children: [
                   Expanded(
-                    flex: 2,
+                      flex: 2,
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: TextButton(
                             onPressed: rightLudo,
-                            child: Image.asset('assets/dice$leftDiceNumber.jpg')),
+                            child:
+                                Image.asset('assets/dice$leftDiceNumber.jpg')),
                       )),
-
                   Expanded(
-                    flex: 2,
+                      flex: 2,
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: TextButton(
                             onPressed: leftLudo,
-                            child: Image.asset('assets/dice$rightDiceNumber.jpg')
-                        ),
+                            child:
+                                Image.asset('assets/dice$rightDiceNumber.jpg')),
                       )),
-
-
                 ],
-
               ),
-
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   ElevatedButton(
                       style: ElevatedButton.styleFrom(
-                          primary: Colors.pinkAccent.withOpacity(0.6)
-                      ),
-                      onPressed: (){
+                          primary: Colors.pinkAccent.withOpacity(0.6)),
+                      onPressed: () {
                         setState(() {
                           leftLudo();
-
                         });
                       },
                       child: Padding(
@@ -84,24 +79,24 @@ class _HomePageState extends State<HomePage> {
                       )),
                   ElevatedButton(
                       style: ElevatedButton.styleFrom(
-                          primary: Colors.pinkAccent.withOpacity(0.6)
-                      ),
-                      onPressed: (){
+                          primary: Colors.pinkAccent.withOpacity(0.6)),
+                      onPressed: () {
                         setState(() {
                           rightLudo();
-
                         });
                       },
                       child: Padding(
                         padding: const EdgeInsets.all(8.0),
                         child: Text("Change Right"),
                       ))
-
                 ],
               ),
-
-
-
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => FinalPage()));
+                  },
+                  child: Text("Next Page"))
             ],
           ),
         ),
