3dbb5b7d8a411eab1becca02beff18bc7affc718
Juwel Sheikh <jewelshekh1996@gmail.com>
2023-01-29 02:27:54 +0600

    mul div

--- lib/final_page.dart
+++ lib/final_page.dart
@@ -10,6 +10,12 @@ class FinalPage extends StatefulWidget {
 class _FinalPageState extends State<FinalPage> {
   double index = 0;
   double index2 = 1;
+  double index3=50;
+  double div=0;
+  double mul=1;
+  double mul1=0;
+  double index5=0;
+  TextEditingController textEditingController = TextEditingController();
 
   void count() {
     setState(() {
@@ -17,18 +23,33 @@ class _FinalPageState extends State<FinalPage> {
     });
   }
 
+
+
   subCount() {
     setState(() {
       index2 += 2;
     });
   }
 
+  divider(){
+    setState(() {
+     div=index3/=2;
+    });
+  }
+
+  multiplication(){
+    setState(() {
+      index5=mul*mul++;
+      mul1++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    TextEditingController textEditingController = TextEditingController();
+
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Colors.white60,
+        backgroundColor: Colors.black,
         body: Container(
           child: Center(
             child: Column(
@@ -44,18 +65,7 @@ class _FinalPageState extends State<FinalPage> {
                               backgroundColor: Colors.black),
                           autofocus: true,
                           onPressed: () {},
-                          child: Text("Button 1")),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextButton(
-                          style: TextButton.styleFrom(
-                              backgroundColor: Colors.green,
-                              primary: Colors.red,
-                              shadowColor: Colors.black),
-                          autofocus: true,
-                          onPressed: () {},
-                          child: Text("Button 1")),
+                          child: Text("তাসকিনের গণিত বই")),
                     ),
                   ],
                 ),
@@ -65,12 +75,24 @@ class _FinalPageState extends State<FinalPage> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          Text(
-                            'সংখ্যা হল জোড়=${index}',
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold),
+                          Container(
+                            height:40,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(Radius.circular(12),
+
+                                ),
+                                color: Colors.blue
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.only(left: 14,right: 14,top: 8,bottom: 8),
+                              child: Text(
+                                "সংখ্যা হল জোড়=${index}",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
                           ),
                           ElevatedButton(
                               onPressed: () {
@@ -86,19 +108,97 @@ class _FinalPageState extends State<FinalPage> {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                          TextButton(
-                            onPressed: () { },
-                            child: Text(
-                              "সংখ্যা হল বিজোড়=${index2}",
-                              style: TextStyle(
-                                  color: Colors.blue,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.bold),
+                          Container(
+                            height:40,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(12),
+
+                              ),
+                              color: Colors.white.withOpacity(0.8)
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "সংখ্যা হল বিজোড়=${index2}",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold),
+                              ),
                             ),
                           ),
                           ElevatedButton(
                               onPressed: () {
                                 subCount();
+                              },
+                              child: Icon(
+                                Icons.description,
+                                color: Colors.white,
+                                size: 22,
+                              ))
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Container(
+                            height:40,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.all(Radius.circular(12),
+
+                                ),
+                                color: Colors.white
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "ভাগফল হলো= ${index3}/2 = ${div}",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                          ElevatedButton(
+                              onPressed: () {
+                                divider();
+                              },
+                              child: Icon(
+                                Icons.description,
+                                color: Colors.white,
+                                size: 22,
+                              ))
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Expanded(
+                            child: Container(
+                              margin: EdgeInsets.only(left: 30,right: 30),
+                              height:40,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.all(Radius.circular(12),
+
+                                  ),
+                                  color: Colors.blue.withOpacity(0.4)
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  "বর্গফল  ${(mul1)}= $index5",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          ),
+                          ElevatedButton(
+                              onPressed: () {
+                                multiplication();
                               },
                               child: Icon(
                                 Icons.description,
